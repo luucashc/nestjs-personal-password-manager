f9f56eef8a52bdc589052b70109d79b7bdd111eb
luucashc <lucashgcardoso@gmail.com>
2021-10-04 12:08:23 -0300

    chore: new service

--- src/password/password.module.ts
+++ src/password/password.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PasswordController } from './password.controller';
+import { PasswordService } from './password.service';
 
 @Module({
-  controllers: [PasswordController]
+  controllers: [PasswordController],
+  providers: [PasswordService]
 })
 export class PasswordModule {}
