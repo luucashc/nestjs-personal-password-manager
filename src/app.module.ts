1dbe0b0e51916468f23b727bf8293dc767c3aa98
luucashc <lucashgcardoso@gmail.com>
2021-10-04 11:44:09 -0300

    chore: new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PasswordModule } from './password/password.module';
 
 @Module({
-  imports: [],
+  imports: [PasswordModule],
 })
 export class AppModule {}
